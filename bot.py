e80766036890927dcfe736c6f59180bed2c62e3f
Ashpan Raskar <araskar@redhat.com>
2022-05-31 14:24:39 -0400

    Changed to Slash commands. Updated direction of leaderboard

--- bot.py
+++ bot.py
@@ -1,44 +1,47 @@
 # bot.py
-import os, discord, datetime, sys, asyncio, logging
+import os
+import discord
+import logging
+import asyncio
 from discord.ext import commands
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 
 logger = logging.getLogger("discord")
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-handler.setFormatter(
-    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
-)
+handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
 logger.addHandler(handler)
 
-load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
-PREFIX = os.getenv("PREFIX")
+config = dict(dotenv_values(".env"))
+TOKEN = config["DISCORD_TOKEN"]
+PREFIX = config["PREFIX"]
+
+
+class WordleBot(commands.Bot):
+    def __init__(self, *, intents: discord.Intents, command_prefix):
+        super().__init__(intents=intents, command_prefix=command_prefix)
+
+    async def setup_hook(self):
+        await bot.load_extension("cogs.wordle_cog")
+
+
 intents = discord.Intents.all()
 intents.members = True
 
-bot = commands.Bot(
-    command_prefix=commands.when_mentioned_or(PREFIX),
-    intents=intents,
-    chunk_guilds_at_startup=True,
-    message_commands=True,
-)
+bot = WordleBot(intents=intents, command_prefix=PREFIX)
+
 
 @bot.event
 async def on_ready():
     print("Logged in as {0.user}".format(bot))
-    await bot.change_presence(
-        activity=discord.Activity(name="Wordling", type=5)
-    )
+    await bot.change_presence(activity=discord.Activity(name="Wordling", type=5))
 
 
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
-        await ctx.send(
-            "You're missing permissions to do that :neutral_face:", delete_after=5
-        )
+        await ctx.send("You're missing permissions to do that :neutral_face:", delete_after=5)
 
 
-bot.load_extension("cogs.wordle_cog")
-bot.run(TOKEN)
+if __name__ == "__main__":
+    asyncio.run(bot.start(TOKEN))
